52c2825b710efbecbfae7f1a39d1955839e697b1
Ashe <molly.02.andrews@gmail.com>
2023-02-08 23:33:29 -0500

    Inclding SpiritX3 works for Linux

---
+++ main.cpp
@@ -0,0 +1,12 @@
+#include <boost/spirit/home/x3.hpp>
+#include <iostream>
+
+using std::cout;
+using std::endl;
+using std::string;
+
+int main(void) {
+    cout << "Hello world!" << endl;
+    return 0;
+}
+
---
+++ makefile
@@ -0,0 +1,2 @@
+main_linux: main.cpp
+	c++ -I /usr/local/boost_1_81_0 - Wall main.cpp -o main.out
