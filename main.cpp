89aeb58034884aadccd80d1583a1b80dfe2fe991
Ashe <molly.02.andrews@gmail.com>
2023-02-12 23:23:22 -0500

    Created parser function for arrays of ints

--- main.cpp
+++ main.cpp
@@ -5,8 +5,49 @@ using std::cout;
 using std::endl;
 using std::string;
 
+template <typename Iterator>
+bool arrayOfIntegersParser(Iterator, Iterator);
+
 int main(void) {
-    cout << "Hello world!" << endl;
+    cout << "Enter and array of integers, such as [1, 2, 3]" << endl;
+    cout << "Type q or Q to quit" << endl;
+
+    string str;
+
+    while (getline(std::cin, str))
+    {
+        if (str.empty() || str[0] == 'q' || str[0] == 'Q')
+            break;
+
+        if (arrayOfIntegersParser(str.begin(), str.end()))
+        {
+            cout << "Parsing succeeded\n";
+        }
+        else
+        {
+            std::cout << "Parsing failed\n";
+        }
+    }
+
+    std::cout << "Bye... :)\n";
+
     return 0;
 }
 
+template <typename Iterator>
+bool arrayOfIntegersParser(Iterator first, Iterator last) {
+    using boost::spirit::x3::int_;
+    using boost::spirit::x3::char_;
+    using boost::spirit::x3::phrase_parse;
+    using boost::spirit::x3::ascii::space;
+
+    bool result = phrase_parse(
+        first,                          //  Start Iterator
+        last,                           //  End Iterator
+        char_('[') >> int_ >> *(',' >> int_) >> char_(']'),   //  The Parser
+        space                           //  The Skip-Parser
+    );
+    if (first != last) // fail if we did not get a full match
+        return false;
+    return result;
+}
