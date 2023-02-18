9262829e6bdcff5610cadb4fb76940cd860e0f7b
Ashe <molly.02.andrews@gmail.com>
2023-02-18 16:54:13 -0500

    Added empty set to integer array parser

--- main.cpp
+++ main.cpp
@@ -8,8 +8,11 @@ using std::string;
 template <typename Iterator>
 bool arrayOfIntegersParser(Iterator, Iterator);
 
+/*template <typename Iterator>
+bool arrayOfBoolsParser(Iterator, Iterator);*/
+
 int main(void) {
-    cout << "Enter an array of integers, such as [1, 3, 5]" << endl;
+    cout << "Enter an array of integers, such as [2, 3, 7]" << endl;
     cout << "Type q or Q to quit" << endl;
 
     string str;
@@ -36,6 +39,7 @@ int main(void) {
 
 template <typename Iterator>
 bool arrayOfIntegersParser(Iterator first, Iterator last) {
+//TODO: Add support for empty set
     using boost::spirit::x3::int_;
     using boost::spirit::x3::char_;
     using boost::spirit::x3::phrase_parse;
@@ -44,10 +48,39 @@ bool arrayOfIntegersParser(Iterator first, Iterator last) {
     bool result = phrase_parse(
         first,                          //  Start Iterator
         last,                           //  End Iterator
-        char_('[') >> int_ >> *(',' >> int_) >> char_(']'),   //  The Parser [x, y, z]
+        (
+                '[' >> int_
+                >> *(',' >> int_) >> ']'
+            |   "[]"
+        ),
         space                           //  The Skip-Parser
     );
     if (first != last) // fail if we did not get a full match
         return false;
     return result;
 }
+
+/*template <typename Iterator>
+bool arrayOfBoolsParser(Iterator first, Iterator last) {
+    using boost::spirit::x3::string;
+    using boost::spirit::x3::char_;
+    using boost::spirit::x3::phrase_parse;
+    using boost::spirit::x3::ascii::space;
+
+    bool result = phrase_parse(
+        first,                          //  Start Iterator
+        last,
+        //  Begin grammar
+        (
+                '[' >> string("true")
+                >> *(',' >> string("true")) >> ']'
+            |   "[]" 
+        ),
+            //  End grammar                           //  End Iterator
+        //schar_('[') >> string("true") | string("false") >> *(',' >> string("true")) >> char_(']'),   //  The Parser [true, false, false]
+        space                           //  The Skip-Parser
+    );
+    if (first != last) // fail if we did not get a full match
+        return false;
+    return result;
+}*/
