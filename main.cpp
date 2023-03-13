72643298d09bcd6c1cf7ac54c47b6bd97b27a1ad
Ashe <molly.02.andrews@gmail.com>
2023-03-13 14:02:21 -0400

    Moved double array parser to main.cpp for neatness

--- main.cpp
+++ main.cpp
@@ -9,6 +9,9 @@ template <typename Iterator>
 bool arrayOfIntegersParser(Iterator, Iterator);
 
 template <typename Iterator>
+bool arrayOfDoublesParser(Iterator, Iterator);
+
+template <typename Iterator>
 bool arrayOfBoolsParser(Iterator, Iterator);
 
 int main(void) {
@@ -22,7 +25,7 @@ int main(void) {
         if (str.empty() || str[0] == 'q' || str[0] == 'Q')
             break;
 
-        if (arrayOfBoolsParser(str.begin(), str.end()))
+        if (arrayOfDoublesParser(str.begin(), str.end()))
         {
             cout << "Parsing succeeded\n";
         }
@@ -76,6 +79,24 @@ bool arrayOfBoolsParser(Iterator first, Iterator last) {
             |   "[]"  //empty set is valid
         ), 
         //End grammar
+        space                           //  The Skip-Parser
+    );
+    if (first != last) // fail if we did not get a full match
+        return false;
+    return result;
+}
+
+template <typename Iterator>
+bool arrayOfDoublesParser(Iterator first, Iterator last) {
+    using boost::spirit::x3::double_;
+    using boost::spirit::x3::char_;
+    using boost::spirit::x3::phrase_parse;
+    using boost::spirit::x3::ascii::space;
+
+    bool result = phrase_parse(
+        first,                          //  Start Iterator
+        last,                           //  End Iterator
+        char_('[') >> double_ >> *(',' >> double_) >> char_(']'),   //  The Parser
         space                           //  The Skip-Parser
     );
     if (first != last) // fail if we did not get a full match
