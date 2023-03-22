0c91a69b46e649e95d3a040f19785bd24dae293e
silverywater <catgirl2359@gmail.com>
2023-03-22 17:47:42 -0400

    string parser

--- main.cpp
+++ main.cpp
@@ -5,11 +5,13 @@ using std::cout;
 using std::endl;
 using std::string;
 
+// template <typename Iterator>
+// bool arrayOfIntegersParser(Iterator, Iterator);
 template <typename Iterator>
-bool arrayOfIntegersParser(Iterator, Iterator);
+bool arrayOfStringsParser(Iterator, Iterator);
 
 int main(void) {
-    cout << "Enter and array of integers, such as [1, 2, 3]" << endl;
+    cout << "Enter and array of strings such as ['a','b','c']"<< endl;//integers, such as [1, 2, 3]" << endl;
     cout << "Type q or Q to quit" << endl;
 
     string str;
@@ -19,7 +21,7 @@ int main(void) {
         if (str.empty() || str[0] == 'q' || str[0] == 'Q')
             break;
 
-        if (arrayOfIntegersParser(str.begin(), str.end()))
+        if (arrayOfStringsParser(str.begin(), str.end()))
         {
             cout << "Parsing succeeded\n";
         }
@@ -30,9 +32,44 @@ int main(void) {
     }
 
     std::cout << "Bye... :)\n";
-
     return 0;
 }
+
+// template <typename Iterator>
+// bool arrayOfIntegersParser(Iterator first, Iterator last) {
+//     using boost::spirit::x3::int_;
+//     using boost::spirit::x3::char_;
+//     using boost::spirit::x3::phrase_parse;
+//     using boost::spirit::x3::ascii::space;
+
+//     bool result = phrase_parse(
+//         first,                          //  Start Iterator
+//         last,                           //  End Iterator
+//         char_('[') >> int_ >> *(',' >> int_) >> char_(']'),   //  The Parser
+//         space                           //  The Skip-Parser
+//     );
+//     if (first != last) // fail if we did not get a full match
+//         return false;
+//     return result;
+// }
+
+// template <typename Iterator>
+// bool arrayOfCharsParser(Iterator first, Iterator last) {
+//     using boost::spirit::x3::int_;
+//     using boost::spirit::x3::char_;
+//     using boost::spirit::x3::phrase_parse;
+//     using boost::spirit::x3::ascii::space;
+
+//     bool result = phrase_parse(
+//         first,                          //  Start Iterator
+//         last,                           //  End Iterator
+//         char_('[') >> (char_) >> *(',' >> char_) >> char_(']'),   //  The Parser
+//         space                           //  The Skip-Parser
+//     );
+//     if (first != last) // fail if we did not get a full match
+//         return false;
+//     return result;
+// }
 
 template <typename Iterator>
 bool arrayOfIntegersParser(Iterator first, Iterator last) {
@@ -45,6 +82,26 @@ bool arrayOfIntegersParser(Iterator first, Iterator last) {
         first,                          //  Start Iterator
         last,                           //  End Iterator
         char_('[') >> int_ >> *(',' >> int_) >> char_(']'),   //  The Parser
+        space                           //  The Skip-Parser
+    );
+    if (first != last) // fail if we did not get a full match
+        return false;
+    return result;
+}
+
+template <typename Iterator>
+bool arrayOfStringsParser(Iterator first, Iterator last) {
+    using boost::spirit::x3::int_;
+    using boost::spirit::x3::char_;
+    using boost::spirit::x3::phrase_parse;
+    using boost::spirit::x3::ascii::space;
+
+    //auto const quoted_string = boost::spirit::x3::lexeme['"' >> +(char_ - '"') >> '"'];
+
+    bool result = phrase_parse(
+        first,                          //  Start Iterator
+        last,                           //  End Iterator
+        char_('[') >> char_('\'') >> *(char_ - char('\'')) >> char_('\'') >> *(',' >> char_('\'') >> *(char_ - char('\'')) >> char_('\''))>> char_(']'),   //  The Parser
         space                           //  The Skip-Parser
     );
     if (first != last) // fail if we did not get a full match
